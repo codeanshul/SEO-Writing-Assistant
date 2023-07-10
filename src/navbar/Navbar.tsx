5c1dfba8d516f494a84d987d2d3272e0f56e15fb
anshul.goyal <anshul.goyal@sprinklr.com>
2023-07-10 13:37:47 +0530

    added padding bottom

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <img src="Sprinklr_Logo.webp" alt="Sprinklr Logo" width="150" height="45" className="d-inline-block align-text-top" loading='lazy'/>
-          <h1 className="main-heading"> SEO Checker Tool </h1>
+          <h1 className="main-heading"> SEO Writing Assistant </h1>
         </div>
       </nav>
     </>
